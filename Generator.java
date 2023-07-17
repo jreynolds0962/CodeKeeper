27ef7ce34e7fa67567c63d00f97f23126d58358b
Joshua Reynolds <j.reynolds0962@gmail.com>
2023-07-17 11:28:50 -0500

    return list of random characters

--- Generator.java
+++ Generator.java
@@ -6,14 +6,23 @@ import java.util.Random;
 
 public class Generator {
 
-    public char genPassword(String special) {
+    public ArrayList<Character> genPassword(String special) {
+        
+        Random rand = new Random();
         List<Character> noSpecials = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','0','1','2','3','4','5','6','7','8','9'));
         // List<Character> withSpecials = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','0','1','2','3','4','5','6','7','8','9','?','!','-','*'));
+        ArrayList<Character> newPassword = new ArrayList<Character>();
 
-        Random rand = new Random();
-        char randomItem = noSpecials.get(rand.nextInt(noSpecials.size()));
+        for (int i = 0; i <10; i ++) {
+            
+            char randomItem = noSpecials.get(rand.nextInt(noSpecials.size()));
+            newPassword.add(randomItem);
 
-        return randomItem;
+        }
+
+        
+
+        return newPassword;
     
     }
     public static void main(String[] args) {
